18386580b4eb4bc4f473a43696e07a8ec81a49b2
Bruno Cordoba <cordobabruno32@gmail.com>
2024-07-10 01:53:55 -0300

    se realizo el ejercicio 15

---
+++ js/15-ejercicio15.js
@@ -0,0 +1,13 @@
+const palabras = prompt("escribe una frase: ").toLowerCase();
+const vocales = "aeiou";
+let cantVocal = 0;
+
+for (let i = 0; i < palabras.length; i++) {
+  for (let j = 0; j < vocales.length; j++) {
+    if (palabras[i] === vocales[j]) {
+      cantVocal++;
+    }
+  }
+}
+
+document.write(`su frase tiene ${cantVocal} vocales`);
