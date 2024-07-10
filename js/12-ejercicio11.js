d11f83766cdd2c0a4368ee6c4e583f2aa718df38
Bruno Cordoba <cordobabruno32@gmail.com>
2024-07-10 01:31:41 -0300

    se realizo el ejercicio 12 del tp

---
+++ js/12-ejercicio11.js
@@ -0,0 +1,4 @@
+// 12- Realiza un script que genere un número aleatorio entre 1 y 99
+const numeroAletorio = Math.floor(Math.random() * 99) + 1;
+  
+alert(`tu numero aleatorio es ${numeroAletorio}`)
